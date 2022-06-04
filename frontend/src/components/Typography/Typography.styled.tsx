fcaa49b6bd767296437ca95cc60f2c4008d66cc9
Piotr Rynio <pwrynio@gmail.com>
2022-06-04 14:08:32 +0200

    update Typography.tsx

---
+++ frontend/src/components/Typography/Typography.styled.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const heading3 = styled.h3`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 1.2;
+  margin: 0;
+`;
+
+const heading4 = styled.h4`
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.2;
+  margin: 0;
+`;
+
+const body = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.5;
+  margin: 0;
+`;
+
+const button = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.5;
+  margin: 0;
+`;
--- frontend/src/components/Typography/Typography.tsx
+++ frontend/src/components/Typography/Typography.tsx
@@ -1,8 +1,20 @@
 export interface TypographyProps {
   children: React.ReactNode;
-  variant: 'title' | 'subtitle' | 'body';
+  variant: 'title' | 'subtitle' | 'body' | 'button';
 }
 
 export const Typography = ({ children, variant = 'body' }: TypographyProps) => {
+  if (variant === 'title') {
+    return <h3>{children}</h3>;
+  }
+  if (variant === 'subtitle') {
+    return <h4>{children}</h4>;
+  }
+  if (variant === 'button') {
+    return <p>{children}</p>;
+  }
+  if (variant === 'body') {
+    return <p>{children}</p>;
+  }
   return <p>{children}</p>;
 };
