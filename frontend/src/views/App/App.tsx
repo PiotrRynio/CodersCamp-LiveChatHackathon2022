186d8df2e28f8cfbe01b3c7eeba3405d8210d826
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-06-04 19:43:01 +0200

    Merge branch 'main' of https://github.com/PiotrRynio/CodersCamp-LiveChatHackathon2022 into Test_summary
    # Conflicts:
    #	frontend/src/views/App/App.tsx

--- frontend/src/views/App/App.tsx
+++ frontend/src/views/App/App.tsx
@@ -3,14 +3,13 @@ import { Quiz } from 'views';
 import { MainContent, Wrapper } from './App.styled';
 import { TopBar } from 'components';
 import { Footer } from 'components';
-import { TestSummary } from '../../components/TestSummary/TestSummary';
 
 export const App = () => {
   return (
     <Wrapper>
       <TopBar />
       <MainContent>
-        <TestSummary result={'100%'} />
+        <Quiz />
       </MainContent>
       <Footer />
     </Wrapper>
