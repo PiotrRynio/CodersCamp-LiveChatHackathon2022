66add318096401a0754dbfe798b19a259b71b59a
AgnieszkaPrzybylowska <agnieszka.przybylowska1@gmail.com>
2022-06-04 19:11:41 +0200

    Test summary (#27)
    * Initial view
    
    * add icons

---
+++ frontend/src/components/ShareIcons/ShareIcons.tsx
@@ -0,0 +1,18 @@
+import { IconContainer, Wrapper } from './ShareIcons.styled';
+import { SocialIcon } from 'react-social-icons';
+
+export const ShareIcons = () => {
+  return (
+    <Wrapper>
+      <IconContainer>
+        <SocialIcon network="facebook" bgColor="white" style={{ height: 80, width: 80 }} />
+      </IconContainer>
+      <IconContainer>
+        <SocialIcon network="linkedin" bgColor="white" style={{ height: 80, width: 80 }} />
+      </IconContainer>
+      <IconContainer>
+        <SocialIcon network="twitter" bgColor="white" style={{ height: 80, width: 80 }} />
+      </IconContainer>
+    </Wrapper>
+  );
+};
