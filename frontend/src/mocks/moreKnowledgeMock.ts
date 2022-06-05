6e9f53c17885f1edacaaadfef31a8b07cc7f29ca
Piotr Rynio <pwrynio@gmail.com>
2022-06-05 07:12:18 +0200

    update mocks

--- frontend/src/mocks/moreKnowledgeMock.ts
+++ frontend/src/mocks/moreKnowledgeMock.ts
@@ -1,14 +1,29 @@
 export const moreKnowledgeMock = {
   articleList: [
     {
-      title: 'Księga Kapitana Bomby',
-      text: 'Z tej księgi poznasz wszystkie zasady Gwiezdnej floty',
-      link: 'https://www.youtube.com/channel/UCpts-8H1DqUaGmUkFhbXMQQ',
+      title: 'Badania NASK:',
+      text: 'Ponad połowa polskich internautów styka się z manipulacją i dezinformacją w internecie',
+      link: 'https://www.nask.pl/pl/aktualnosci/2249,Badania-NASK-ponad-polowa-polskich-internautow-styka-sie-z-manipulacja-i-dezinfo.html',
     },
     {
-      title: 'Księga Ulicy',
-      text: '"Jeśli trafisz na silniejszego, przyjmij z pokorą wpierdol od niego" - oraz inne mądrości z miejskiej jungle',
-      link: 'https://blokekipa.fandom.com/wiki/Ksi%C4%99ga_Ulicy',
+      title: 'Stop dezinformacji',
+      text: 'Przewodnik dla dziennikarzy i redakcji',
+      link: 'https://panoptykon.org/stop-dezinformacji-przewodnik',
+    },
+    {
+      title: 'Mitologia współczesna',
+      text: 'Uważaj, w co wierzysz!',
+      link: 'https://mitologiawspolczesna.pl/',
+    },
+    {
+      title: 'Whois Lookup',
+      text: 'Uważaj, w co wierzysz!',
+      link: 'https://whois.domaintools.com/',
+    },
+    {
+      title: 'Wojna, trolle i fake newsy',
+      text: 'Czyli na co zwracać uwagę w mediach społecznościowych.',
+      link: 'https://publicystyka.ngo.pl/zanim-podamy-dalej-post',
     },
   ],
 };
